31215a3c384ab7d8ae6349ce07e056f2326666fc
m19e <nexus.me7290@gmail.com>
2020-11-10 19:35:53 +0900

    Apply block module to utils method

--- sprite/block.go
+++ sprite/block.go
@@ -1,14 +1,54 @@
 package sprite
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"image"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/m19e/tiny-side-scroll/utils"
+)
+
+const (
+	block_img = `++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++
+++++++++++++++++`
+	blockWidth  = 16
+	blockHeight = 16
+)
+
+var (
+	blockImg *ebiten.Image
+)
 
 type Block struct {
 	BaseSprite
 }
 
-func NewBlock(images []*ebiten.Image) *Block {
+func init() {
+	tmpImage := image.NewRGBA(image.Rect(0, 0, blockWidth, blockHeight))
+	utils.CreateImageFromString(block_img, tmpImage)
+	blockImg = ebiten.NewImage(blockWidth, blockHeight)
+	blockImg.ReplacePixels(tmpImage.Pix)
+
+}
+
+func NewBlock() *Block {
 	block := new(Block)
-	block.Images = images
-	block.ImageNum = len(images)
+	block.Images = []*ebiten.Image{
+		blockImg,
+	}
+	block.ImageNum = len(block.Images)
 	return block
 }
