fd36dde85a78abaf99e6941db441b40a7d2224d3
m19e <nexus.me7290@gmail.com>
2020-11-15 22:54:49 +0900

    Recieve color arg

--- utils/main.go
+++ utils/main.go
@@ -10,18 +10,18 @@ var (
 	Green = &color.RGBA{uint8(15), uint8(56), uint8(15), 0xff}
 )
 
-func CreateImageFromString(charString string, img *image.RGBA) {
+func CreateImageFromString(charString string, img *image.RGBA, color *color.RGBA) {
 	width := img.Rect.Size().X
 	for indexY, line := range strings.Split(charString, "\n") {
 		for indexX, str := range line {
 			pos := 4*indexY*width + 4*indexX
 			if string(str) == "+" {
-				img.Pix[pos] = uint8(15)   // R
-				img.Pix[pos+1] = uint8(56) // G
-				img.Pix[pos+2] = uint8(15) // B
-				img.Pix[pos+3] = 0xff      // A
+				img.Pix[pos] = color.R
+				img.Pix[pos+1] = color.G
+				img.Pix[pos+2] = color.B
+				img.Pix[pos+3] = color.A
 			} else {
-				img.Pix[pos] = 0   // R
+				img.Pix[pos] = 0   // R	
 				img.Pix[pos+1] = 0 // G
 				img.Pix[pos+2] = 0 // B
 				img.Pix[pos+3] = 0 // A
