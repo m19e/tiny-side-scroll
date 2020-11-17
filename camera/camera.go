d5a48a107d319e34850153a02c116740c8fc0769
m19e <nexus.me7290@gmail.com>
2020-11-18 04:58:27 +0900

    Implement camera module

--- camera/camera.go
+++ camera/camera.go
@@ -3,4 +3,41 @@ package camera
 type Camera struct {
 	X int
 	Y int
+
+	Width     int
+	Height    int
+	MaxWidth  int
+	MaxHeight int
+
+	//XLeftLimit  int
+	//XRightLimit int
+	//YUpperLimit int
+	//YLowerLimit int
+
+	XCenter int
+	YCenter int
+}
+
+func (c *Camera) Move(x, y int) {
+	maxXOffset := -(c.MaxHeight - c.Width)
+	maxYOffset := -(c.MaxWidth - c.Height)
+
+	restX := c.XCenter - x
+	restY := c.YCenter - y
+
+	if restX > 0 {
+		c.X = 0
+	} else if restX < maxXOffset {
+		c.X = maxXOffset
+	} else {
+		c.X = restX
+	}
+
+	if restY > 0 {
+		c.Y = 0
+	} else if restY < maxYOffset {
+		c.Y = maxYOffset
+	} else {
+		c.Y = restY
+	}
 }
