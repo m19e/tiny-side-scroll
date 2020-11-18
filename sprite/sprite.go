95878c1d9f14229d2b1134bc85114849244af3a8
m19e <nexus.me7290@gmail.com>
2020-11-18 15:57:28 +0900

    Check cross each object

--- sprite/sprite.go
+++ sprite/sprite.go
@@ -64,44 +64,12 @@ func (s *BaseSprite) GetCoordinates() (int, int, int, int) {
 	return s.Position.X, s.Position.Y, w, h
 }
 
-func isOverlap(x1, x2, x3, x4 int) bool {
-	if x1 <= x4 && x2 >= x3 {
-		return true
-	}
-	return false
-}
+func (s *BaseSprite) Intersect(object Sprite) bool {
+	ax, ay, aw, ah := s.GetCoordinates()
+	bx, by, bw, bh := object.GetCoordinates()
 
-func (s *BaseSprite) detectCollisions(object Sprite, dx, dy *int, camera *camera.Camera) *CollideMap {
-	var cm CollideMap
-	x := s.Position.X
-	y := s.Position.Y
-	img := s.currentImage()
-	w, h := img.Size()
-
-	x1, y1, w1, h1 := object.GetCoordinates()
-
-	x1 += camera.X
-	y1 += camera.Y + 1 // +1 for land correctly
-
-	overlappedX := isOverlap(x, x+w, x1, x1+w1)
-	overlappedY := isOverlap(y, y+h, y1, y1+h1)
-
-	if overlappedY {
-		if *dx < 0 && x+*dx <= x1+w1 && x+w+*dx >= x1 {
-			cm.Left = true
-		} else if *dx > 0 && x+w+*dx >= x1 && x+*dx <= x1+w1 {
-			cm.Right = true
-		}
-	}
-	if overlappedX {
-		if *dy < 0 && y+*dy <= y1+w1 && y+h+*dy >= y1 {
-			cm.Top = true
-		} else if *dy > 0 && y+h+*dy >= y1 && y+*dy <= y1+h1 {
-			cm.Bottom = true
-		}
-	}
-
-	return &cm
+	// a left-top < b right-bottom && a right-bottom > b left-top
+	return (ax < bx+bw && ay < by+bh) && (ax+aw > bx && ay+ah > by)
 }
 
 func (s *BaseSprite) IsCollide(object Sprite, dx, dy *int, camera *camera.Camera) {
